9cc608d12a1e7e3a8dd0077d3bcd10747771e60b
hardinas <104479410+hardinas@users.noreply.github.com>
2024-07-31 13:44:05 +0700

    updated

--- resources/views/components/in.blade.php
+++ resources/views/components/in.blade.php
@@ -1,4 +1,4 @@
-<div class="relative h-full overflow-hidden rounded-lg flex flex-col bg-green-400" data-carousel="slide" data-carousel-interval="{{ $setting->duration * 1000 }}">
+<div class="relative z-0 h-full overflow-hidden rounded-lg flex flex-col bg-green-400" data-carousel="slide" data-carousel-interval="{{ $setting->duration * 1000 }}">
     @foreach ($datas as $key => $item)
         <div class="{{$key <> 0 ? 'hidden' : ''}} duration-2000 ease-in-out" data-carousel-item="">
             @if ($item['type'] == 'image')
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[IndexController::class,'in']);
-Route::get('/out',[IndexController::class,'out']);
+Route::get('/',[IndexController::class,'in'])->name('in');
+Route::get('/out',[IndexController::class,'out'])->name('out');
 
