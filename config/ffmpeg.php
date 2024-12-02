16005a9b5113fab9d0a12420ea11f8a8c79d1055
hardinas <104479410+hardinas@users.noreply.github.com>
2024-12-02 16:10:38 +0700

    add config stream

---
+++ config/ffmpeg.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'host' => env('FFMPEG_HOST','192.168.9.190'),
+    'port' => env('FFMPEG_PORT','80'),
+    'username' => env('FFMPEG_USERNAME','sysuno'),
+    'password' => env('FFMPEG_PASSWORD','bdsyst3m'),
+];
