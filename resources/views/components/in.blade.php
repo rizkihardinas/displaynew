824b8f0e58a98b833e3e407873da737ed42f7686
hardinas <104479410+hardinas@users.noreply.github.com>
2024-12-05 15:31:41 +0700

    Update in.blade.php

--- resources/views/components/in.blade.php
+++ resources/views/components/in.blade.php
@@ -3,7 +3,7 @@
         <div class="{{$key <> 0 ? 'hidden' : ''}} duration-2000 ease-in-out" data-carousel-item="">
             @if ($item['type'] == 'image')
                 {{-- <img class="object-fill margin-0 w-full h-full" src="data:image/png;base64,{{ $item['path'] }}" alt="..."> --}}
-                <img class="object-fill margin-0 w-full h-full" src="{{ asset('Logo_Operator.jpg') }}" alt="...">
+                <img class="object-fill margin-0 w-full h-full" src="{{ asset('public/Logo_Operator.jpg') }}" alt="...">
             @else
             <video src="data:video/mp4;base64,{{ $item['path'] }}" loop autoplay class="object-fill w-96 h-96" id="video"></video>
             @endif
