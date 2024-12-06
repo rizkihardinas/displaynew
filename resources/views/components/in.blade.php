1b52a5bac2e5b358f1c5250dfb4addf674f4e1f7
hardinas <104479410+hardinas@users.noreply.github.com>
2024-12-06 10:03:56 +0700

    Update in.blade.php

--- resources/views/components/in.blade.php
+++ resources/views/components/in.blade.php
@@ -1,6 +1,6 @@
 <div class="relative z-0 h-full overflow-hidden rounded-lg flex flex-col bg-green-400" data-carousel="slide" data-carousel-interval="{{ $setting->duration * 1000 }}">
     @foreach ($datas as $key => $item)
-        <div class="{{$key <> 0 ? 'hidden' : ''}} duration-2000 ease-in-out" data-carousel-item="">
+        <div class="{{$key <> 0 ? 'hidden' : ''}} duration-2000 ease-in-out absolute inset-0 transition-transform transform z-20 translate-x-0" data-carousel-item="">
             @if ($item['type'] == 'image')
                 <img class="object-fill margin-0 w-full h-full" src="data:image/png;base64,{{ $item['path'] }}" alt="...">
                 {{-- <img class="object-fill margin-0 w-full h-full" src="{{ asset('1.jpg') }}" alt="..."> --}}
