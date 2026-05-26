03acefddc431d7fd4b26aa59edef98e20c0197b3
Rizki Hardinas Permana <36495798+rizkihardinas@users.noreply.github.com>
2026-05-26 15:45:56 +0700

    Update lpr.blade.php

--- resources/views/components/lpr.blade.php
+++ resources/views/components/lpr.blade.php
@@ -55,7 +55,7 @@
     
 </div> --}}
 <div class="relative z-0 h-full overflow-hidden rounded-lg flex flex-col bg-green-400" data-carousel="slide" data-carousel-interval="{{ $setting->duration * 1000 }}">
-@foreach ($datas_operator as $key => $item)
+    @foreach ($datas as $key => $item)
     <div class="{{$key <> 0 ? 'hidden' : ''}} duration-2000 ease-in-out absolute inset-0 transition-transform transform z-20 translate-x-0" data-carousel-item="">
         @if ($item['type'] == 'image')
             @if ($item['hasEnc'])
