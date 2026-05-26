9ea579fbb1e89e5a7c63741d46e167e3bda31ff2
Rizki Hardinas Permana <36495798+rizkihardinas@users.noreply.github.com>
2026-05-26 15:47:41 +0700

    Update lpr.blade.php

--- resources/views/components/lpr.blade.php
+++ resources/views/components/lpr.blade.php
@@ -1,4 +1,4 @@
-<div class="px-2 py-1 h-full flex flex-col {{ config('uno.style.primary') }}" > --}}
+{{-- <div class="px-2 py-1 h-full flex flex-col {{ config('uno.style.primary') }}" > --}}
     {{-- <span class="bg-[{{ config('uno.style.primary') }}] my-1 text-2xl text-white px-1 font-bold hidden" id="labelin">OUT</span> --}}
     {{-- <div class="flex-grow  flex items-center justify-center"> --}}
         
@@ -54,24 +54,7 @@
     </div>
     
 </div> --}}
-{{-- <div class="relative z-0 h-full overflow-hidden rounded-lg flex flex-col bg-green-400" data-carousel="slide" data-carousel-interval="{{ $setting->duration * 1000 }}"> --}}
-    @foreach ($datas as $key => $item)
-    <div class="{{$key <> 0 ? 'hidden' : ''}} duration-2000 ease-in-out absolute inset-0 transition-transform transform z-20 translate-x-0" data-carousel-item="">
-        @if ($item['type'] == 'image')
-            @if ($item['hasEnc'])
-            <img class="object-fill margin-0 w-full h-full" src="data:image/png;base64,{{ $item['path'] }}" alt="...">    
-            @else
-            <img class="object-fill margin-0 w-full h-full" src="{{ $item['path'] }}" alt="...">    
-            @endif
-            
-            {{-- <img class="object-fill margin-0 w-full h-full" src="{{ asset('1.jpg') }}" alt="..."> --}}
-        @else
-        <video src="data:video/mp4;base64,{{ $item['path'] }}" loop autoplay class="object-fill w-96 h-96" id="video"></video>
-        @endif
-
-    </div>
-@endforeach
-</div>
+test
 
 
 @push('scripts')
