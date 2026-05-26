72c49d259953613cfa7d026600e46cc5ad22c4bb
Rizki Hardinas Permana <36495798+rizkihardinas@users.noreply.github.com>
2026-05-26 16:49:39 +0700

    Update lpr.blade.php

--- resources/views/components/lpr.blade.php
+++ resources/views/components/lpr.blade.php
@@ -1,56 +1,34 @@
 {{-- <div class="px-2 py-1 h-full flex flex-col"> --}}
-<div class="relative z-0 h-full overflow-hidden rounded-lg flex flex-col bg-green-400"
-        data-carousel="slide"
-        data-carousel-interval="{{ $setting->duration * 1000 }}">
+<div class="relative z-0 h-full overflow-hidden rounded-lg flex flex-col bg-green-400" data-carousel="slide"
+    data-carousel-interval="{{ $setting->duration * 1000 }}">
 
-        @foreach ($datas_operator as $key => $item)
-
-            <div class="{{ $key <> 0 ? 'hidden' : '' }}
+    @foreach ($datas_operator as $key => $item)
+        <div class="{{ $key != 0 ? 'hidden' : '' }}
                 duration-2000 ease-in-out absolute inset-0 transition-transform transform z-20 translate-x-0"
-                data-carousel-item="">
+            data-carousel-item="">
 
-                @if ($item['type'] == 'image')
-
-                    @if ($item['hasEnc'])
-
-                        <img
-                            class="object-fill margin-0 w-full h-full"
-                            src="data:image/png;base64,{{ $item['path'] }}"
-                            alt="...">
-
-                    @else
-
-                        <img
-                            class="object-fill margin-0 w-full h-full"
-                            src="{{ $item['path'] }}"
-                            alt="...">
-
-                    @endif
-
+            @if ($item['type'] == 'image')
+                @if ($item['hasEnc'])
+                    <img class="object-fill margin-0 w-full h-full" src="data:image/png;base64,{{ $item['path'] }}"
+                        alt="...">
                 @else
-
-                    <video
-                        src="data:video/mp4;base64,{{ $item['path'] }}"
-                        loop
-                        autoplay
-                        muted
-                        playsinline
-                        class="object-fill w-full h-full"
-                        id="video">
-
-                    </video>
-
+                    <img class="object-fill margin-0 w-full h-full" src="{{ $item['path'] }}" alt="...">
                 @endif
+            @else
+                <video src="data:video/mp4;base64,{{ $item['path'] }}" loop autoplay muted playsinline
+                    class="object-fill w-full h-full" id="video">
 
-            </div>
+                </video>
+            @endif
 
-        @endforeach
+        </div>
+    @endforeach
 
-    </div>
+</div>
 <div id="wrapper_data" class="hidden">
     <div class="flex-none {{ config('uno.style.primary') }} py-1 mt-1 text-center">
-        <img class=" w-full h-96 object-contain hidden" alt="" id="imagein"
-                src="{{ asset('Logo_Operator.jpg') }}">
+        <img class=" w-full h-64 object-contain hidden" alt="" id="imagein"
+            src="{{ asset('Logo_Operator.jpg') }}">
     </div>
     <div class="flex-none {{ config('uno.style.primary') }} py-1 mt-1 text-center">
         <span class="text-3xl" id="lpr">&nbsp;</span>
