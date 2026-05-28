88b9eb70419da874cf464f1c1e1b58abcfd3d89a
Rizki Hardinas Permana <36495798+rizkihardinas@users.noreply.github.com>
2026-05-28 15:49:18 +0700

    Update out.blade.php

--- resources/views/components/out.blade.php
+++ resources/views/components/out.blade.php
@@ -1,5 +1,5 @@
-<div class="flex flex-col hidden" id="page-out">
-    <div class="{{ config('uno.style.primary') }} border-4 border-black p-2">
+<div class="flex flex-col hidden {{ config('uno.style.primary') }}" id="page-out">
+    <div class=" border-4 border-black p-2">
         <div class="{{ config('uno.style.primary') }} flex-grow text-center flex items-center justify-center">
             {{-- <img class="w-full h-[130px] object-contain" alt="" id="image"> --}}
             <img class="w-full h-full object-contain" alt="" id="image">
@@ -7,7 +7,7 @@
                 {{-- QRIS Template --}}
                 <div class="w-full max-w-xs py-12 bg-white rounded shadow-md overflow-hidden border border-gray-200">
                     {{-- Header: QRIS + GPN logo --}}
-                    <div class="flex items-center justify-between px-12 pt-3 pb-2 border-b border-gray-200">
+                    <div class="flex items-center justify-between px-12 py-12 pb-2 border-b border-gray-200">
                         <img src="{{ asset('assets/qris.png') }}" alt="QRIS" class="h-8 w-auto">
                         <img src="{{ asset('assets/gpn.svg') }}" alt="GPN" class="h-8 w-auto">
                     </div>
@@ -17,7 +17,7 @@
                     </div>
                     {{-- Footer --}}
                 </div>
-                <span id="expired" class="text-red-500 font-bold mt-2"></span>
+                <span id="expired" class="text-red-500 font-bold my-6"></span>
             </div>
         </div>
     </div>
@@ -43,25 +43,25 @@
             </tr>
         </table>
     </div>
-    <div class="{{ config('uno.style.primary') }} p-2 font-bold  text-xl">
+    <div class="{{ config('uno.style.primary') }} p-2 font-bold  text-4xl">
         <table class="w-full">
             <tr>
-                <td class="text-left" width="150">Lama Parkir</td>
+                <td class="text-left" width="200">Lama Parkir</td>
                 <td>:</td>
-                <td class="text-right text-2xl" id="duration">0 Jam 0 Menit</td>
+                <td class="text-right text-6xl" id="duration">0 Jam 0 Menit</td>
             </tr>
             <tr>
-                <td class="text-left" width="150">Total</td>
+                <td class="text-left" width="200">Total</td>
                 <td>:</td>
-                <td class="text-right text-6xl" id="total">0</td>
+                <td class="text-right text-8xl" id="total">0</td>
             </tr>
             <tr id="informasi-pembayaran-row" class="hidden">
-                <td class="text-left" width="150" id="informasi-pembayaran-title">[Informasi Pembayaran]</td>
+                <td class="text-left" width="200" id="informasi-pembayaran-title">[Informasi Pembayaran]</td>
                 <td>:</td>
                 <td class="text-right text-4xl" id="informasi-pembayaran"></td>
             </tr>
             <tr id="informasi-member-row" class="hidden">
-                <td class="text-left" width="150" id="informasi-pembayaran-title">Masa Aktif Member</td>
+                <td class="text-left" width="200" id="informasi-pembayaran-title">Masa Aktif Member</td>
                 <td>:</td>
                 <td class="text-right text-4xl" id="informasi-member"></td>
             </tr>
