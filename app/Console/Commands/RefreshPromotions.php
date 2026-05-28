a6c2fe19013b468bd1de41ee82b86dfc5bc4f399
Rizki Hardinas Permana <36495798+rizkihardinas@users.noreply.github.com>
2026-05-28 08:11:08 +0700

    Update RefreshPromotions.php

--- app/Console/Commands/RefreshPromotions.php
+++ app/Console/Commands/RefreshPromotions.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Cache;
+use App\Models\Promotion;
 
 class RefreshPromotions extends Command
 {
@@ -17,7 +18,12 @@ class RefreshPromotions extends Command
         Cache::forget('promotions_with_operator');
 
         $this->info('Promotions cache cleared successfully.');
-
+        Cache::rememberForever('promotions_no_operator', function () {
+            return Promotion::whereNull('is_operator')->get();
+        });
+        Cache::rememberForever('promotions_with_operator', function () {
+            return Promotion::whereNotNull('is_operator')->get();
+        });
         return 0;
     }
 }
